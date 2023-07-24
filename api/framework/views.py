299005e304bf33f727699d28358c96eabe64e051
Felipe Cabrera <fecabrera@protonmail.com>
2023-07-24 15:58:02 -0400

    Adds `.get_queryset()` to `api.framework.views.APIView`

--- api/framework/views.py
+++ api/framework/views.py
@@ -1,15 +1,52 @@
+from django.db.models import QuerySet
 from rest_framework.views import *  # noqa
 
 
 class APIView(APIView):
+    queryset = None
     serializer_class = None
+
+    def get_queryset(self):
+        """
+        Get the list of items for this view.
+        This must be an iterable, and may be a queryset.
+        Defaults to using `self.queryset`.
+
+        This method should always be used rather than accessing `self.queryset`
+        directly, as `self.queryset` gets evaluated only once, and those results
+        are cached for all subsequent requests.
+
+        You may want to override this if you need to provide different
+        querysets depending on the incoming request.
+
+        (Eg. return a list of items that is specific to the user)
+        """
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method."
+            % self.__class__.__name__
+        )
+
+        queryset = self.queryset
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            queryset = queryset.all()
+        return queryset
 
     def get_serializer_class(self):
         return self.serializer_class
 
-    def get_serializer(self, *args, **kwargs):
-        kwargs['context'] = {
-            'request': self.request
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
         }
+
+    def get_serializer(self, *args, **kwargs):
         serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
         return serializer_class(*args, **kwargs)
