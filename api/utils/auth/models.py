9c937a17e17853eea5106c88962ee87f049dd5a6
Felipe Cabrera <fecabrera@protonmail.com>
2025-05-05 20:54:49 -0400

    Add OTPBase model for two-factor authentication support

--- api/utils/auth/models.py
+++ api/utils/auth/models.py
@@ -1,3 +1,4 @@
+import pyotp
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils.translation import gettext_lazy as _
@@ -16,3 +17,28 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):  # pragma: no cover
 
     class Meta:
         abstract = True
+
+
+class OTPBase(models.Model):
+    class Meta:
+        abstract = True
+
+    otp_secret = models.CharField(max_length=32)
+    verified = models.BooleanField(default=False)
+
+    def generate(self):
+        if not self.otp_secret:
+            self.otp_secret = pyotp.random_base32()
+
+        self.verified = False
+        self.save()
+
+        totp = pyotp.TOTP(self.otp_secret, interval=300)
+        return totp.now()
+
+    def verify(self, otp_code):
+        totp = pyotp.TOTP(self.otp_secret, interval=300)
+        verified = totp.verify(otp_code, valid_window=1)
+        self.verified = verified
+        self.save()
+        return verified
