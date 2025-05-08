3e95e85ca34b12ba081420c76dd40eda12103a93
Felipe Cabrera <fecabrera@protonmail.com>
2025-05-08 02:51:19 -0400

    Remove unused queryset and filtering methods from APIView

--- api/framework/views.py
+++ api/framework/views.py
@@ -3,39 +3,8 @@ from rest_framework.views import *  # noqa
 
 
 class APIView(APIView):
-    queryset = None
     serializer_class = None
     query_params_serializer_class = None
-
-    # The filter backend classes to use for queryset filtering
-    filter_backends = api_settings.DEFAULT_FILTER_BACKENDS
-
-    def get_queryset(self):
-        """
-        Get the list of items for this view.
-        This must be an iterable, and may be a queryset.
-        Defaults to using `self.queryset`.
-
-        This method should always be used rather than accessing `self.queryset`
-        directly, as `self.queryset` gets evaluated only once, and those results
-        are cached for all subsequent requests.
-
-        You may want to override this if you need to provide different
-        querysets depending on the incoming request.
-
-        (Eg. return a list of items that is specific to the user)
-        """
-        assert self.queryset is not None, (
-            "'%s' should either include a `queryset` attribute, "
-            "or override the `get_queryset()` method."
-            % self.__class__.__name__
-        )
-
-        queryset = self.queryset
-        if isinstance(queryset, QuerySet):
-            # Ensure queryset is re-evaluated on each request.
-            queryset = queryset.all()
-        return queryset
 
     def get_serializer_class(self):
         """
@@ -97,16 +66,3 @@ class APIView(APIView):
             'format': self.format_kwarg,
             'view': self
         }
-
-    def filter_queryset(self, queryset):
-        """
-        Given a queryset, filter it with whichever filter backend is in use.
-
-        You are unlikely to want to override this method, although you may need
-        to call it either from a list view, or from a custom `get_object`
-        method if you want to apply the configured filtering backend to the
-        default queryset.
-        """
-        for backend in list(self.filter_backends):
-            queryset = backend().filter_queryset(self.request, queryset, self)
-        return queryset
