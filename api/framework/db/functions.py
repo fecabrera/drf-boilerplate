0e40fe8d4ee8409353a94a841a85948e66b320fb
Felipe Cabrera <fecabrera@protonmail.com>
2025-05-18 20:46:35 -0400

    Refactor database function classes for improved reusability
    Replaced `AtTimeZone` template logic with a more generic `BinaryFunc` base class to enhance code reuse and clarity. Introduced a new `Timezone` function class for better modularity and alignment with coding standards.

--- api/framework/db/functions.py
+++ api/framework/db/functions.py
@@ -1,6 +1,27 @@
 from django.db.models import Func
 
 
-class AtTimeZone(Func):
+class BinaryFunc(Func):
+    template = '%(lhs)s %(function)s %(rhs)s'
+    arity = 2
+
+    def as_sql(self, compiler, connection, function=None, template=None, **extra_context):
+        left_expr, right_expr = self.get_source_expressions()
+
+        lhs, lhs_params = compiler.compile(left_expr)
+        rhs, rhs_params = compiler.compile(right_expr)
+
+        template = template or self.template
+        function = function or self.function
+
+        data = {'function': function, 'lhs': lhs, 'rhs': rhs}
+        params = lhs_params + rhs_params
+        return template % data, params
+
+
+class AtTimeZone(BinaryFunc):
     function = 'AT TIME ZONE'
-    template = "%(expressions)s %(function)s '%(timezone)s'"
+
+
+class Timezone(Func):
+    function = 'TIMEZONE'
