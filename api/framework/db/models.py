03388a567d9650ae37eea3392921a08de79e12c9
Felipe Cabrera <fecabrera@protonmail.com>
2025-05-23 21:22:49 -0400

    Add abstract base model with validation and save logic
    Introduce an abstract `Model` class with `is_valid` and overridden `save` methods. This ensures validation logic is centralized and invoked before saving instances. It serves as a reusable base for other models.

---
+++ api/framework/db/models.py
@@ -0,0 +1,40 @@
+from django.db.models import *  # noqa
+
+
+class Model(Model):
+    class Meta:
+        abstract = True
+
+    def is_valid(self, raise_exception: bool = False):
+        """
+        Determines whether the current instance or configuration is valid.
+
+        This method performs a validation check and returns a boolean based on
+        the validity of the instance or configuration being evaluated. Optionally,
+        it can raise an exception if the instance is not valid by enabling the
+        `raise_exception` parameter. Specific validation logic is implemented
+        within the method.
+
+        Args:
+            raise_exception (bool): A flag indicating whether to raise an exception
+                if the instance is found to be invalid. Defaults to False.
+
+        Returns:
+            bool: True if the instance is valid; otherwise, False.
+        """
+        return True
+
+    def save(self, *args, **kwargs):
+        """
+        Saves the current model instance after performing validation.
+
+        This method first validates the instance by calling `is_valid` with
+        `raise_exception=True`. If the validation passes, it proceeds to save
+        the instance to the database using the parent class's save method.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.is_valid(raise_exception=True)
+        super().save(*args, **kwargs)
