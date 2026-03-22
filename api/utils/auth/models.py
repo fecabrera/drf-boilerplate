e2380e26efb4cbb85513aa88de96a291ba59402b
Felipe Cabrera <fecabrera0@outlook.com>
2026-03-22 14:46:21 -0300

    Enhance OTP model with timestamp fields and methods for generation, verification, and usage

--- api/utils/auth/models.py
+++ api/utils/auth/models.py
@@ -2,6 +2,7 @@ import pyotp
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 
 class BaseUser(AbstractBaseUser, PermissionsMixin):  # pragma: no cover
@@ -26,25 +27,66 @@ class OTPBase(models.Model):  # pragma: no cover
     TOTP_INTERVAL = None
 
     otp_secret = models.CharField(_('otp secret'), max_length=32)
+    generated_at = models.DateTimeField(blank=True, null=True)
     verified = models.BooleanField(_('verified'), default=False)
+    verified_at = models.DateTimeField(blank=True, null=True)
+    used_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def generate(self):
+    def generate(self) -> str:
+        """
+        Generates a new OTP code and returns it.
+
+        Returns:
+            str: The OTP code.
+        """
         assert self.TOTP_INTERVAL is not None, _('`{value}` must be set').format(value='TOTP_INTERVAL')
 
-        if not self.otp_secret:
+        if not self.generated_at:
             self.otp_secret = pyotp.random_base32()
+            self.generated_at = timezone.now()
 
         self.verified = False
+        self.verified_at = None
+        self.used_at = None
         self.save()
 
         totp = pyotp.TOTP(self.otp_secret, interval=self.TOTP_INTERVAL)
         return totp.now()
 
-    def verify(self, otp_code):
+    def verify(self, otp_code: str) -> bool:
+        """
+        Verifies the OTP code and returns True if it is valid, False otherwise.
+
+        Args:
+            otp_code: The OTP code to verify.
+
+        Returns:
+            bool: True if the OTP code is valid, False otherwise.
+        """
         assert self.TOTP_INTERVAL is not None, _('`{value}` must be set').format(value='TOTP_INTERVAL')
+        assert self.generated_at is not None, _('This OTP is not generated.')
+        assert self.verified_at is None, _('This OTP has already been verified.')
+        assert self.used_at is None, _('This OTP has already been used.')
 
         totp = pyotp.TOTP(self.otp_secret, interval=self.TOTP_INTERVAL)
         verified = totp.verify(otp_code, valid_window=1)
         self.verified = verified
+        self.verified_at = timezone.now() if verified else None
         self.save()
         return verified
+    
+    def use(self) -> None:
+        """
+        Marks the OTP code as used by setting the used_at field to the current time.
+
+        Raises:
+            AssertionError: If the OTP code is not verified.
+            AssertionError: If the OTP code has already been used.
+        """
+        assert self.used_at is None, _('This OTP has already been used.')
+        assert self.verified_at is not None, _('This OTP is not verified.')
+        assert self.generated_at is not None, _('This OTP is not generated.')
+
+        self.used_at = timezone.now()
+        self.save()
